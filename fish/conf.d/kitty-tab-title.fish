d5915524c9d8bda69c9ec0c5e9e753c7c8d0f940
Jack Hsu <jack.hsu@gmail.com>
2026-05-12 13:31:19 -0400

    update

--- fish/conf.d/kitty-tab-title.fish
+++ fish/conf.d/kitty-tab-title.fish
@@ -1,5 +1,8 @@
-# Auto-set kitty tab title to current directory basename on cd
-if set -q KITTY_WINDOW_ID
+# Auto-set kitty tab title to current directory basename on cd.
+# Skip inside tmux: `kitty @` writes escape sequences to the controlling TTY
+# and waits for a reply, but tmux intercepts the bytes so the call blocks
+# until response-timeout. That blocks fish startup and leaves a blank pane.
+if set -q KITTY_WINDOW_ID; and not set -q TMUX
     function __kitty_tab_title --on-variable PWD
         string match -q '*/tmp*' $PWD; and return
         kitty @ set-tab-title (basename $PWD)
